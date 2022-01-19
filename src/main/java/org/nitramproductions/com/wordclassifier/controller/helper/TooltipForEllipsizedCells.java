188ecb2851d4ad7df9adfd41c69983d152229af6
XXNitram <matti.55@hotmail.de>
2022-01-19 03:00:42 +0100

    closes #33; Add tooltips for ellipsized cells (when content is too long)
    Signed-off-by: XXNitram <matti.55@hotmail.de>

---
+++ src/main/java/org/nitramproductions/com/wordclassifier/controller/helper/TooltipForEllipsizedCells.java
@@ -0,0 +1,36 @@
+package org.nitramproductions.com.wordclassifier.controller.helper;
+
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.Tooltip;
+import javafx.scene.text.Text;
+
+public class TooltipForEllipsizedCells<T> extends TableCell<T, String> {
+
+    private BooleanProperty itemTextLongerThanColumnWidthProperty;
+
+    public TooltipForEllipsizedCells() {
+
+    }
+
+    @Override
+    protected void updateItem(final String item, final boolean empty) {
+        super.updateItem(item, empty);
+        setText(item);
+        TableColumn<T, String> tableColumn = getTableColumn();
+        double itemTextWidth = new Text(item + "  ").getLayoutBounds().getWidth();
+        boolean itemTextLongerThanColumnWidth = tableColumn.getWidth() < itemTextWidth;
+        itemTextLongerThanColumnWidthProperty = new SimpleBooleanProperty(itemTextLongerThanColumnWidth);
+        tableColumn.widthProperty().addListener((observableValue, oldSelection, newSelection) ->
+                itemTextLongerThanColumnWidthProperty.set(tableColumn.widthProperty().get() < itemTextWidth));
+        tooltipProperty().bind(Bindings
+                .when(Bindings
+                        .and(itemTextLongerThanColumnWidthProperty, Bindings
+                                .or(emptyProperty().not(), itemProperty().isNotNull())))
+                .then(new Tooltip(item))
+                .otherwise((Tooltip) null));
+    }
+}
