73a1e5afcacd5e423293719ced691f919aa32aad
XXNitram <matti.55@hotmail.de>
2022-01-26 00:28:55 +0100

    Add cancel button functionality
    Signed-off-by: XXNitram <matti.55@hotmail.de>

--- src/main/java/org/nitramproductions/com/wordclassifier/controller/ExportToCSVController.java
+++ src/main/java/org/nitramproductions/com/wordclassifier/controller/ExportToCSVController.java
@@ -1,6 +1,8 @@
 package org.nitramproductions.com.wordclassifier.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
@@ -56,7 +58,9 @@ public class ExportToCSVController {
     }
 
     @FXML
-    private void onCancelButtonClick() {
-
+    private void onCancelButtonClick(ActionEvent event) {
+        Node node = (Node) event.getSource();
+        Stage stage = (Stage) node.getScene().getWindow();
+        stage.close();
     }
 }
