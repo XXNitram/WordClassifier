5fdf3037a59664d3c299cde9103d604f3dcadb88
XXNitram <matti.55@hotmail.de>
2022-01-26 00:10:26 +0100

    Connect exportToCSV view to ExportToCSVController
    Signed-off-by: XXNitram <matti.55@hotmail.de>

--- src/main/java/org/nitramproductions/com/wordclassifier/controller/ExportToCSVController.java
+++ src/main/java/org/nitramproductions/com/wordclassifier/controller/ExportToCSVController.java
@@ -1,4 +1,39 @@
 package org.nitramproductions.com.wordclassifier.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+
 public class ExportToCSVController {
+
+    @FXML
+    private CheckBox groupNameCheckBox;
+    @FXML
+    private CheckBox groupDateModifiedCheckBox;
+    @FXML
+    private CheckBox groupCreationDateCheckBox;
+    @FXML
+    private Button groupSelectSaveDestinationButton;
+    @FXML
+    private TextField groupSaveDestinationTextField;
+
+    public ExportToCSVController() {
+
+    }
+
+    @FXML
+    private void initialize() {
+
+    }
+
+    @FXML
+    private void onGroupExportButtonClick() {
+
+    }
+
+    @FXML
+    private void onCancelButtonClick() {
+
+    }
 }
