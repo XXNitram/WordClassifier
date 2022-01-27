a666d4fd83fa050f032c66c312d409a22163dff8
XXNitram <matti.55@hotmail.de>
2022-01-27 20:28:33 +0100

    closes #51; First rudimentary error handling, show error alert for every unchecked exception
    Signed-off-by: XXNitram <matti.55@hotmail.de>

--- src/main/java/org/nitramproductions/com/wordclassifier/MainApplication.java
+++ src/main/java/org/nitramproductions/com/wordclassifier/MainApplication.java
@@ -1,8 +1,10 @@
 package org.nitramproductions.com.wordclassifier;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
@@ -13,6 +15,7 @@ public class MainApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
+        Thread.setDefaultUncaughtExceptionHandler(MainApplication::showError);
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("controller/main.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("WordClassifier");
@@ -22,6 +25,19 @@ public class MainApplication extends Application {
         stage.show();
         stage.setMinWidth(stage.getWidth());
         stage.setMinHeight(stage.getHeight());
+    }
+
+    private static void showError(Thread t, Throwable e) {
+        if (Platform.isFxApplicationThread()) {
+            showErrorAlert();
+        } else {
+            System.err.println("An unexpected error occurred in " + t);
+        }
+    }
+
+    private static void showErrorAlert() {
+        Alert alert = new Alert(Alert.AlertType.ERROR, "Es ist ein Fehler aufgetreten!");
+        alert.showAndWait();
     }
 
     public static void main(String[] args) {
