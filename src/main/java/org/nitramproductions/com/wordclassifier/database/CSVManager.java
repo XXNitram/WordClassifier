681e45f4d966c4efbe282185f1427c418c7db01a
XXNitram <matti.55@hotmail.de>
2022-01-28 19:23:26 +0100

    Add function to read groups from a csv file and insert the data into the database
    Signed-off-by: XXNitram <matti.55@hotmail.de>

--- src/main/java/org/nitramproductions/com/wordclassifier/database/CSVManager.java
+++ src/main/java/org/nitramproductions/com/wordclassifier/database/CSVManager.java
@@ -5,6 +5,8 @@ import org.nitramproductions.com.wordclassifier.database.helper.Columns;
 import org.nitramproductions.com.wordclassifier.database.helper.QueryHelper;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -34,6 +36,24 @@ public class CSVManager {
         String query = queryHelper.getSpecificBelongsToColumnsQuery(columns);
         try (Connection connection = DataSource.getConnection()) {
             new Csv().write(connection, filePath, query, null);
+        }
+    }
+
+    public void readGroupsFromCSV(String filePath) throws SQLException {
+        ResultSet resultSet = new Csv().read(filePath, null, null);
+        if (!resultSet.next()) {
+            throw new IllegalStateException();
+        }
+        String query = "INSERT INTO \"GROUP\" VALUES (?, COALESCE(?, CURRENT_TIMESTAMP()), COALESCE(?, CURRENT_TIMESTAMP()))";
+        try (Connection connection = DataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            do {
+                preparedStatement.setObject(1, resultSet.getString("NAME"));
+                preparedStatement.setObject(2, resultSet.getString("DATE_MODIFIED"));
+                preparedStatement.setObject(3, resultSet.getString("CREATION_DATE"));
+                preparedStatement.addBatch();
+            } while (resultSet.next());
+            preparedStatement.executeBatch();
         }
     }
 }
