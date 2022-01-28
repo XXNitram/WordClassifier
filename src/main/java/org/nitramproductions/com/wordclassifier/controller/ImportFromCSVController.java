2eaea9de813f66675e5ce40b2117d8a42972c7cd
XXNitram <matti.55@hotmail.de>
2022-01-28 19:27:47 +0100

    Add view for import from csv and the corresponding controller with the assigned fxml fields
    Signed-off-by: XXNitram <matti.55@hotmail.de>

---
+++ src/main/java/org/nitramproductions/com/wordclassifier/controller/ImportFromCSVController.java
@@ -0,0 +1,46 @@
+package org.nitramproductions.com.wordclassifier.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+
+public class ImportFromCSVController {
+
+    @FXML
+    private RadioButton groupRadioButton;
+    @FXML
+    private RadioButton expressionRadioButton;
+    @FXML
+    private RadioButton belongToRadioButton;
+    @FXML
+    private Label fileLocationLabel;
+
+    public ImportFromCSVController() {
+
+    }
+
+    @FXML
+    private void initialize() {
+
+    }
+
+    @FXML
+    private void onSelectFileLocationButtonClick() {
+
+    }
+
+    @FXML
+    private void onFormatRequirementsHyperlinkClick() {
+
+    }
+
+    @FXML
+    private void onImportButtonClick() {
+
+    }
+
+    @FXML
+    private void onCancelButtonClick() {
+
+    }
+}
