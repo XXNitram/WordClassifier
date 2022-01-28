5304f54b861c82284739b92b396ca8006c8b7d76
XXNitram <matti.55@hotmail.de>
2022-01-29 00:51:24 +0100

    Set toggle group and add function to select file location
    Signed-off-by: XXNitram <matti.55@hotmail.de>

--- src/main/java/org/nitramproductions/com/wordclassifier/controller/ImportFromCSVController.java
+++ src/main/java/org/nitramproductions/com/wordclassifier/controller/ImportFromCSVController.java
@@ -1,8 +1,16 @@
 package org.nitramproductions.com.wordclassifier.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import org.nitramproductions.com.wordclassifier.database.CSVManager;
+
+import java.awt.event.ActionEvent;
+import java.io.File;
 
 public class ImportFromCSVController {
 
@@ -15,18 +23,32 @@ public class ImportFromCSVController {
     @FXML
     private Label fileLocationLabel;
 
+    private String fileLocation;
+    private final ToggleGroup toggleGroup = new ToggleGroup();
+    private final CSVManager csvManager = new CSVManager();
+
     public ImportFromCSVController() {
 
     }
 
     @FXML
     private void initialize() {
-
+        groupRadioButton.setToggleGroup(toggleGroup);
+        expressionRadioButton.setToggleGroup(toggleGroup);
+        belongToRadioButton.setToggleGroup(toggleGroup);
     }
 
     @FXML
-    private void onSelectFileLocationButtonClick() {
-
+    private void onSelectFileLocationButtonClick(ActionEvent event) {
+        Node node = (Node) event.getSource();
+        Stage stage = (Stage) node.getScene().getWindow();
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("CSV Dateien", "*.csv"));
+        File selectedFile = fileChooser.showOpenDialog(stage);
+        if (selectedFile != null) {
+            fileLocation = selectedFile.getAbsolutePath();
+            fileLocationLabel.setText(fileLocation);
+        }
     }
 
     @FXML
