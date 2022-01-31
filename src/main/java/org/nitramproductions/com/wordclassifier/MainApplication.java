1da4eb54a02bdda8ad0fec61ce32d56edd2ef47c
XXNitram <matti.55@hotmail.de>
2022-01-31 02:03:04 +0100

    Read preferences and set scene depending on stored values
    Signed-off-by: XXNitram <matti.55@hotmail.de>

--- src/main/java/org/nitramproductions/com/wordclassifier/MainApplication.java
+++ src/main/java/org/nitramproductions/com/wordclassifier/MainApplication.java
@@ -11,22 +11,30 @@ import org.nitramproductions.com.wordclassifier.controller.MainController;
 
 import java.io.IOException;
 import java.util.Objects;
+import java.util.prefs.Preferences;
 
 public class MainApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
         Thread.setDefaultUncaughtExceptionHandler(MainApplication::showError);
+
+        Preferences preferences = Preferences.userRoot().node("/wordclassifier");
+        double stageWidth = preferences.getDouble("WINDOW_WIDTH", 800);
+        double stageHeight = preferences.getDouble("WINDOW_HEIGHT", 600);
+        boolean stageMaximized = preferences.getBoolean("WINDOW_MAXIMIZED", false);
+
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("controller/main.fxml"));
         fxmlLoader.setControllerFactory(controller -> new MainController(stage));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        Scene scene = new Scene(fxmlLoader.load(), stageWidth, stageHeight);
         stage.setTitle("WordClassifier");
         stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("controller/book-icon.png"))));
+        stage.setMaximized(stageMaximized);
         stage.setScene(scene);
         stage.sizeToScene();
         stage.show();
-        stage.setMinWidth(stage.getWidth());
-        stage.setMinHeight(stage.getHeight());
+        stage.setMinWidth(750);
+        stage.setMinHeight(300);
     }
 
     private static void showError(Thread t, Throwable e) {
